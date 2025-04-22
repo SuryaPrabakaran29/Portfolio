3d7ec6973360925d666d98b5c90981ba59169889
SuryaPrabakaran <suryaprabakaran29@gmail.com>
2025-04-22 08:27:11 +0530

    revert unwanted components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { TodoItemsComponent } from './todo-items/todo-items.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 
 export const routes: Routes = [
     {path:'login', component: LoginComponent},
-    {path:'todoItems', component: TodoItemsComponent},
     {path:'', redirectTo:'login', pathMatch:'full'},
-    { path: 'course-details/:id', component: TodoListComponent }
-
 ];
